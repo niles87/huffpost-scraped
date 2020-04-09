93801b70063d89f35c0c8710e266f7900b88430d
niles87 <nbing19@gmail.com>
2020-04-09 13:24:34 -0700

    added functions for closing modal and added click events for each button

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,23 +1,66 @@
 $(function () {
+  function closeModal() {
+    $("#modal").removeClass("is-visible");
+    $("#note-modal").removeClass("is-visible");
+  }
+
   // Get articles button
-  $(".scrape").on("click", function (event) {
-    $.ajax("/scrape", { type: "GET" }).then(function () {
-      console.log("scraped");
-      setTimeout(function () {
-        location.reload();
-      }, 2000);
-    });
+  $(".scrape").on("click", (event) => {
+    $.ajax({ url: "/scrape", method: "GET" }).then((data) => console.log("scraped"));
   });
 
   // Save button
-  $(document).on("click", ".save", function (event) {
+  $(document).on("click", ".save", (event) => {
     const id = $(this).data("id");
     const saved = $(this).data("saved");
     const savedState = { saved: saved };
     console.log(savedState);
     console.log(saved);
-    $.ajax("/save_article/" + id, { type: "POST", data: savedState }).then(function () {
+    $.ajax({ url: "/save_article/" + id, method: "POST", data: savedState }).then((data) => {
+      location.reload();
       alert("Saved article");
     });
   });
+
+  // Note button
+  $(document).on("click", ".note", (event) => {
+    const id = $(this).data("id");
+    const modalID = $(this).data("open");
+    const title = $(this).data("title");
+    $("#" + modalID).addClass("is-visible");
+    $("#modal-form").attr("data-id", id).attr("data-title", title);
+  });
+
+  $(document).on("click", ".view-note", (event) => {
+    const modalID = $(this).data("open");
+    const id = $(this).data("id");
+    const note = $(this).data("note");
+    console.log(note);
+    $.ajax({ url: "/notes/" + id, method: "GET", data: { id: note } }).then((data) => {
+      $("#" + modalID).addClass("is-visible");
+    });
+  });
+
+  // Submit Note
+  $("#submit").on("click", (event) => {
+    event.preventDefault();
+    $.ajax({
+      url: "/notes",
+      method: "POST",
+      data: {
+        id: $("#modal-form").data("id"),
+        title: $("#modal-form").data("title"),
+        body: $("textarea").val(),
+      },
+    }).then((data) => closeModal());
+  });
+
+  // Delete Note
+  $(document).on("click", ".delete", (event) => {
+    const id = $(this).data("id");
+    $.ajax({ url: "/articles" + id, method: "DELETE" }).then((data) => location.reload());
+  });
+
+  // Close Modal
+  $(".close-modal").on("click", closeModal);
 });
